160d0c01e2c4dbc55569a6698dff14284c3ff17d
josflesan <josue.fle.sanc@gmail.com>
2023-01-31 23:28:06 +0000

    Firebase Local Firestore Setup

--- courier_web_app/lib/src/pages/qrScreen.dart
+++ courier_web_app/lib/src/pages/qrScreen.dart
@@ -1,31 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:qr_flutter/qr_flutter.dart';
 
-class QRScreen extends StatelessWidget {
+const bool useEmulator = true;
+
+class QRScreen extends StatefulWidget {
   const QRScreen(this.title);
 
   final String title;
 
   @override
+  _QRScreenState createState() => _QRScreenState();
+}
+
+class _QRScreenState extends State<QRScreen> {
+  String _username = "";
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  String host = 'localhost'; // change when using Cloud Firestore
+
+  @override
+  void didChangeDependencies() async {
+    if (useEmulator) {
+      db.useFirestoreEmulator(host, 8080);
+      db.settings = const Settings(
+        persistenceEnabled: true,
+      );
+    }
+
+    await db.collection('users').doc('user1').get().then((user) {
+      setState(() {
+        if (user.exists) {
+          this._username = user.data()!['username'];
+        }
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            SizedBox(
-              height: 80,
-            ),
-            SizedBox(
-              height: 80,
-            )
-          ],
-        ),
+        child: Text("User 1 has username $_username"),
       ),
     );
   }
