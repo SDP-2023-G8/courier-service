4df6952b05bfb5310d05eef5f56bda4a25415eb8
josflesan <josue.fle.sanc@gmail.com>
2023-01-31 21:13:08 +0000

    Refactor Project Structure

--- courier_web_app/lib/main.dart
+++ courier_web_app/lib/main.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'pages/qrScreen.dart';
+import 'src/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp();
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'InBoX QR Code',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const QRScreen('1. Please scan the QR code below:'),
-    );
-  }
+  runApp(const CourierBox());
 }
