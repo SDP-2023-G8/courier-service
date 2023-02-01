9578f43f2e1e61befb7bd067f4f2972151118d86
josflesan <josue.fle.sanc@gmail.com>
2023-02-01 17:32:08 +0000

    Adds Confirmation Screen

--- courier_web_app/lib/src/pages/confirmScreen.dart
+++ courier_web_app/lib/src/pages/confirmScreen.dart
@@ -2,26 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 class ConfirmScreen extends StatelessWidget {
-  const ConfirmScreen(this.title);
-
-  final String title;
+  const ConfirmScreen();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            SizedBox(
-              height: 80,
+          children: [
+            FractionallySizedBox(
+              widthFactor: 0.8,
+              child: FittedBox(
+                  fit: BoxFit.contain,
+                  child: Text(
+                    '3. Delivery Complete! You can now close this tab',
+                    style: TextStyle(fontSize: 22),
+                  )),
             ),
-            SizedBox(
-              height: 80,
-            )
+            const SizedBox(
+              height: 50,
+            ),
+            Image.asset("assets/img/check.gif",
+                height: 125.0, width: 125.0, fit: BoxFit.fitWidth)
+            // VERIFIED GIF
           ],
         ),
       ),
