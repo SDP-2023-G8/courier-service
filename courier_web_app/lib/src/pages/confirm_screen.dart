c50de66fc64651f719dc04f30cbbede901daeb17
josflesan <josue.fle.sanc@gmail.com>
2023-02-13 23:27:20 +0000

    Fixes Confirm Screen

--- courier_web_app/lib/src/pages/confirm_screen.dart
+++ courier_web_app/lib/src/pages/confirm_screen.dart
@@ -6,25 +6,26 @@ class ConfirmScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 40, 10, 10),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Expanded(
-                child: Text(
-                  '3. Delivery Complete! You can now close this tab',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 22),
-                ),
-              ),
-              Expanded(
-                child:
-                    Image.asset("assets/img/check.gif", fit: BoxFit.fitWidth),
-              ),
-            ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const FractionallySizedBox(
+              widthFactor: 0.8,
+              child: FittedBox(
+                  fit: BoxFit.cover,
+                  child: Text(
+                    '3. Delivery Complete! You can now close this tab',
+                    style: TextStyle(fontSize: 22),
+                  )),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Image.asset("assets/img/check.gif",
+                height: 125.0, width: 125.0, fit: BoxFit.fitWidth)
+            // VERIFIED GIF
+          ],
         ),
       ),
     );
